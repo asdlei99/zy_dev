fb4ea97abac279be7e742db35c79da5f48e45e53
hunlongyu <hunlongyu@gmail.com>
2020-06-23 16:38:19 +0800

    shortcut

---
+++ src/lib/shortcut/key.js
@@ -0,0 +1,18 @@
+const key = {
+  focus: 'Alt+Space',
+  playAndPause: 'Space',
+  forward: 'Right',
+  back: 'Left',
+  volumeUp: 'Up',
+  volumeDown: 'Down',
+  fullscreen: 'f',
+  escape: 'Esc',
+  next: 'Alt+Right',
+  prev: 'Alt+Left',
+  home: 'Home',
+  end: 'End',
+  opacityUp: 'Alt+Up',
+  opacityDown: 'Alt+Down'
+}
+
+export default key
