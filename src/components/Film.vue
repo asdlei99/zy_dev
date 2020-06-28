70290a4eb6dbf9c64b7e894ca88a5de6359f81cb
hunlongyu <hunlongyu@gmail.com>
2020-06-28 18:12:17 +0800

    完成无限滚动加载

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -28,11 +28,25 @@
         </span>
       </div>
     </div>
-    <div class="body">
+    <div class="body zy-scroll" infinite-wrapper>
       <div class="img">
-        <waterfall :line-gap="200" :watch="items">
-          <waterfall-slot v-for="(i, j) in items" :key="j" :order="j" :width="200" :height="400"></waterfall-slot>
-        </waterfall>
+        <Waterfall :list="list" :gutter="20" :width="240"
+        :breakpoints="{ 1200: { rowPerView: 4 } }"
+        animationEffect="fadeInUp"
+        backgroundColor="rgba(0, 0, 0, 0)"
+        ref="waterfall">
+          <template slot="item" slot-scope="props">
+            <div class="card">
+              <img style="width: 100%" :src="props.data.pic" alt="" @load="$refs.waterfall.refresh()">
+              <div class="name">{{props.data.name}}</div>
+              <div class="info">
+                <span>{{props.data.year}}</span>
+                <span>{{props.data.type}}</span>
+              </div>
+            </div>
+          </template>
+        </Waterfall>
+        <infinite-loading force-use-infinite-wrapper :identifier="infiniteId" @infinite="infiniteHandler"></infinite-loading>
       </div>
       <div class="table"></div>
       <div class="control"></div>
@@ -40,9 +54,9 @@
   </div>
 </template>
 <script>
-import Waterfall from 'vue-waterfall/lib/waterfall'
-import WaterfallSlot from 'vue-waterfall/lib/waterfall-slot'
-import { sites, getSite } from '../lib/site/sites'
+import Waterfall from 'vue-waterfall-plugin'
+import InfiniteLoading from 'vue-infinite-loading'
+import { sites } from '../lib/site/sites'
 import zy from '../lib/site/tools'
 export default {
   name: 'film',
@@ -57,33 +71,66 @@ export default {
       site: {},
       types: [],
       type: {},
-      items: []
+      page: 1,
+      list: [],
+      infiniteId: +new Date()
     }
   },
   components: {
     Waterfall,
-    WaterfallSlot
+    InfiniteLoading
   },
   methods: {
     siteClick (e) {
       this.site = e
-      getSite(e)
-      zy.type(e.key).then(res => {
+      this.list = []
+      this.types = []
+      this.page = 1
+      this.infiniteId += 1
+    },
+    getType () {
+      const key = this.site.key
+      zy.type(key).then(res => {
         this.types = res
         this.type = res[0]
       })
     },
     typeClick (e) {
+      console.log(e, 'type click e')
+      this.list = []
       this.type = e
+      this.page = 1
+      this.infiniteId += 1
+    },
+    getData () {
       const key = this.site.key
-      zy.list(key, 1).then(res => {
-        console.log(res, 'res type click')
-        this.items = res.list
+      zy.list(key, this.page).then(res => {
+        if (res.list.length) {
+          this.page += 1
+          this.list = this.list.concat(res.list)
+          // this.list.push(...res.list)
+        }
+      })
+    },
+    infiniteHandler ($state) {
+      const key = this.site.key
+      const type = this.type.tid
+      console.log(key, this.page, type, 'infinite')
+      zy.list(key, this.page, type).then(res => {
+        console.log(res, 'inadsl')
+        if (res.list.length) {
+          this.page += 1
+          this.list.push(...res.list)
+          $state.loaded()
+        } else {
+          $state.complete()
+        }
       })
     }
   },
   created () {
-    this.site = this.sites[0]
+    this.site = this.sites[1]
+    this.getType()
   }
 }
 </script>
@@ -99,6 +146,7 @@ export default {
     display: flex;
     align-items: center;
     justify-content: space-between;
+    z-index: 10;
   }
   .body{
     margin-top: 20px;
@@ -106,6 +154,50 @@ export default {
     width: 100%;
     background-color: var(--d-bgc-1);
     border-radius: 0 0 5px 5px;
+    // position: relative;
+    overflow-y: scroll;
+    &::-webkit-scrollbar{
+      width: 5px;
+      height: 1px;
+    }
+    &::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      position: absolute;
+    }
+    &::-webkit-scrollbar-track {
+      border-radius: 10px;
+      position: absolute;
+    }
+    .img{
+      // position: absolute;
+      height: 100%;
+      width: 100%;
+      padding: 10px;
+      .card{
+        cursor: pointer;
+        color: var(--d-fc-1);
+        background-color: #111;
+        .name{
+          font-size: 16px;
+          padding: 4px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          white-space: nowrap;
+        }
+        .info{
+          display: flex;
+          justify-content: space-between;
+          font-size: 14px;
+          padding: 4px;
+        }
+        &:hover{
+          box-shadow: var(--d-bsc-hover);
+        }
+      }
+      img{
+        width: 100%;
+      }
+    }
   }
 }
 </style>
